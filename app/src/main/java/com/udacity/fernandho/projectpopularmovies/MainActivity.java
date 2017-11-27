a99a794d28dd89d97827c702bff9ed584838a96c
fdho <fernando_f10@outlook.com>
2017-11-27 16:53:47 -0200

    [MainActivity] defined the strings to access the database of themoviesdb. One for popular movies, the other for toprated movies

--- app/src/main/java/com/udacity/fernandho/projectpopularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/fernandho/projectpopularmovies/MainActivity.java
@@ -1,17 +1,30 @@
 package com.udacity.fernandho.projectpopularmovies;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
     private ProgressDialog pDialog;
     private ListView lv;
+
+    // URL to get contacts JSON
+    private static String urlPopular = "https://api.themoviedb.org/3/movie/popular?api_key=XXX&language=en-US&page=1";
+    private static String urlTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=XXX&language=en-US&page=1";
+    ArrayList<HashMap<String, String>> MoviesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +34,23 @@ public class MainActivity extends AppCompatActivity {
         MoviesList = new ArrayList<>();
         lv = (ListView) findViewById(R.id.list);
         new GetMovies().execute();
+    }
+
+    /**
+     * Using asynchronous task class to retrieve HTTP
+     */
+
+    private class GetMovies extends AsyncTask<Void, Void, Void> {
+
+        //Shows a dialog before complete initialize the apk
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Loading...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
     }
 }
