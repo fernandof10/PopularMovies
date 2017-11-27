1a660279fe5dc50e4cbfa1440802f01c594ce6cf
fdho <fernando_f10@outlook.com>
2017-11-27 16:39:52 -0200

    [MainActivity] overwriten the onCreate method

--- app/src/main/java/com/udacity/fernandho/projectpopularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/fernandho/projectpopularmovies/MainActivity.java
@@ -1,13 +1,25 @@
 package com.udacity.fernandho.projectpopularmovies;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MoviesList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+        new GetMovies().execute();
     }
 }
